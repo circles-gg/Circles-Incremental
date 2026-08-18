ef2064284f177582fc40a1d9910e5dd0b4c827ff
Circles_GG <circlesbusiness1337@gmail.com>
2026-08-18 17:06:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ let circlesPerClick = 1
 let circlesPerSecond = 0
 
 function updateCircles() {
-document.getElementById("circles").textContent = circles;
+    document.getElementById("circles").textContent = circles
 }
 
 function earnCircles() {
-circles += circlesPerClick
-updateCircles()
+    circles += circlesPerClick
+    updateCircles()
+    saveGame()
 }
 
-updateCircles()
+function saveGame() {
+    localStorage.setItem("circles", circles)
+}
+
+function loadGame() {
+    circles = Number(localStorage.getItem("circles")) || 0
+    updateCircles()
+}
+
+loadGame()
