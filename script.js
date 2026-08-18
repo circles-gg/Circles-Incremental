5c0eed6c3f928b3ec6596438f980d9f56474f122
Circles_GG <circlesbusiness1337@gmail.com>
2026-08-18 20:17:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let circles = 0
 let circlesPerClick = 1
 let circlesPerSecond = 0
-let moreCirclesCost = 50
+let moreCirclesCost = 5
 
 function updateCircles() {
-    document.getElementById("circles").textContent = circles
+ document.getElementById("circles").textContent = circles
+document.getElementById("moreCirclesCost").textContent = moreCirclesCost
 }
 
 function earnCircles() {
@@ -13,14 +14,25 @@ function earnCircles() {
     saveGame()
 }
 
+function moreCircles() {
+    if (circles >= moreCirclesCost) {
+        circles -= moreCirclesCost
+        circlesPerClick += 1
+        moreCirclesCost *= 2
+        updateCircles()
+    }
+}
+
 function saveGame() {
 localStorage.setItem("circles", circles)
 localStorage.setItem("circlesPerClick", circlesPerClick)
+localStorage.setItem("moreCirclesCost", moreCirclesCost)
 }
 
 function loadGame() {
 circles = Number(localStorage.getItem("circles")) || 0
 Number(localStorage.getItem("circlesPerClick")) || 1
+Number(localStorage.getItem("moreCirclesCost")) || 5
 updateCircles()
 }
 
