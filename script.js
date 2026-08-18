ade73e47b49629b8e9e5fd8d57b2370bb685efbb
Circles_GG <circlesbusiness1337@gmail.com>
2026-08-18 16:21:49 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let circles = 0
+let circlesPerClick = 1
+let circlesPerSecond = 0
+
+function updateCircles() {
+document.getElementById("circles").textContent = circles;
+}
+
+function earnCircles() {
+circles += circlesPerClick
+updateCircles()
+}
