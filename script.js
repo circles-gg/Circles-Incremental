1108cabb1ee8a441747de057b7f4d18912f83931
Circles_GG <circlesbusiness1337@gmail.com>
2026-08-20 21:28:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function getClickProduction() {
 function getCirclesPerSecond() {
     return (
         getClickProduction() *
-        earnCirclesAutomaticallyLevel * 4
+        earnCirclesAutomaticallyLevel
     )
 }
 
